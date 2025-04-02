3041d3f23d1abffd0a58fa690c7711c43d4bfb83
kangjie-ding <kangjie-ding>
2025-04-02 23:11:44 +0800

    增加了特定数据集loader模块

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,7 +1,25 @@
 import os
 import sys
+import torch
+import torch.utils
+import torch.utils.data
+import torchvision
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(cur_dir, "../"))
 
 
+def get_dataloader(name, root, transform, batch_size=64, split="train", download=False):
+    assert name in ["CelebA", "MNIST", "CIFAR10"], f"{name}:Unknown dataset name!"
+    datasets = None
+    if name=="CelebA":
+        datasets = torchvision.datasets.CelebA(root, transform=transform, split=split, download=download)
+    elif name=="MNIST":
+        datasets = torchvision.datasets.MNIST(root, train=(split=="train"), transform=transform, download=download)
+    elif name=="CIFAR10":
+        datasets = torchvision.datasets.CIFAR10(root, train=(split=="train"), transform=transform, download=download)
+    else:
+        pass
+    assert datasets is not None, "Empty Datasets!"
+    dataloader = torch.utils.data.DataLoader(datasets, batch_size=batch_size, shuffle=True, drop_last=True)
+    return dataloader
