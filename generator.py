095316de0581b078225d205e5d70b5499a6b6466
kangjie-ding <kangjie-ding>
2025-04-02 23:06:48 +0800

    增加了降噪生成步骤

---
+++ generator.py
@@ -0,0 +1,53 @@
+import json
+import os
+import torch
+from models.diffusion import GaussianDiffusion
+from models.Unet import UNetModel
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+def load_config(config_file_path):
+    with open(config_file_path, 'r') as file:
+        model_config = json.load(file)
+    return model_config
+
+if __name__=="__main__":
+    transform2Image = transforms.ToPILImage()
+    # configuring
+    config_file_path = "configs/config.json"
+    model_config = load_config(config_file_path)
+    channels = model_config["data_settings"]["channels"]
+    channel_mul = model_config["model_settings"]["channel_mul_layer"]
+    num_head = model_config["model_settings"]["num_head"]
+    attention_mul = model_config["model_settings"]["attention_mul"]
+    normal_mean = model_config["data_settings"]["normalization_mean"]
+    normal_std = model_config["data_settings"]["normalization_std"]
+    model_save_dir = model_config["path_settings"]["weight_save_dir"]
+    data_name = model_config["data_settings"]["name"]
+    image_size = model_config["data_settings"]["resized_image_size"]
+    model_output_path = os.path.join(model_save_dir, "weights_"+data_name+".pth")
+    print("Configuring Done!")
+
+    # model defining
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    diffusion = GaussianDiffusion(device=device)
+    unet = UNetModel(input_channels=channels, output_channels=channels, channel_mul_layer=channel_mul, num_head=num_head, attention_mul=attention_mul).to(device=device)
+    # weights loading
+    assert os.path.exists(model_output_path), f"{model_output_path} is not existing!"
+    unet.load_state_dict(torch.load(model_output_path))
+    # denoising process
+    unet.eval()
+    with torch.no_grad():
+        generated_images = diffusion.ddim_fashion_sample(unet, image_size, channels, batch_size=16)
+    recoverd_images = (generated_images*normal_std+normal_mean).reshape(4, 4, channels, image_size, image_size)
+    # images showing
+    fig = plt.figure(figsize=(12, 12), constrained_layout=True)
+    gs = fig.add_gridspec(4, 4)
+
+    for row in range(4):
+        for col in range(4):
+            f_ax = fig.add_subplot(gs[row, col])
+            image = transform2Image(recoverd_images[row][col])
+            f_ax.imshow(image, cmap="gray")
+            f_ax.axis("off")
+    plt.show()
