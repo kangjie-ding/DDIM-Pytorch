b2f2d487b947972422b7514a2d7e06b6b9948df8
kangjie-ding <kangjie-ding>
2025-04-02 23:12:39 +0800

    增加了DDIM-fashion的采样

--- models/diffusion.py
+++ models/diffusion.py
@@ -39,7 +39,7 @@ class GaussianDiffusion():
         return a*xt-b*pred_noise
 
     def _pred_pre_xt(self, xt, pred_x0, t):
-        assert (t>1).all(), "time step is required to >1 in this denoising fasion"
+        assert (t>1).all(), "time step is required to >1 in this denoising fashion"
         t = t-1
         pre_t = t-1
         a = (self.sqrt_cumprod_alpha_schedule[pre_t]*self.beta_schedule[t]/(1-self.cumprod_alpha_schedule[t])).reshape(-1, 1, 1, 1)
@@ -50,10 +50,10 @@ class GaussianDiffusion():
         return mean + torch.sqrt(variance)*noise
 
     @torch.no_grad()
-    def ddpm_fasion_sample(self, model, image_size, channels, batch_size=8):
+    def ddpm_fashion_sample(self, model, image_size, channels, batch_size=16):
         device = next(model.parameters()).device
         xt = torch.randn((batch_size, channels, image_size, image_size)).to(device=device)
-        for i in tqdm(range(1, self.time_steps+1)[::-1], desc="denoising process", total=self.time_steps):
+        for i in tqdm(range(1, self.time_steps+1)[::-1], desc="denoising process in DDPM fashion", total=self.time_steps):
             t = torch.full((batch_size, 1), i).to(device=device)
             pred_noise = model(xt, t)
             if i!=1:
@@ -63,9 +63,31 @@ class GaussianDiffusion():
         return xt
 
     @torch.no_grad()
-    def ddim_fasion_sample(self):
-        pass # to do
-    
+    def ddim_fashion_sample(self, model, image_size, channels, sampling_steps=50, sampling_method='uniform', batch_size=16, eta=0.0):
+        device = next(model.parameters()).device
+        if sampling_method=='uniform':
+            assert self.time_steps%sampling_steps==0, "Invalid sampling steps in DDIM, must be divisible by time steps in DDPM!"
+            step_length = self.time_steps//sampling_steps
+            ddim_time_schedule = range(1, self.time_steps+1, step_length)
+            ddim_pre_time = [0]+list(ddim_time_schedule)[:-1]
+        else:
+            pass # to do
+        xt = torch.randn((batch_size, channels, image_size, image_size)).to(device=device)
+        for i in tqdm(range(0, sampling_steps)[::-1], desc="denoising process in DDIM fashion", total=sampling_steps):
+            t = torch.full((batch_size, 1), ddim_time_schedule[i]).to(device=device)
+            pre_t = torch.full((batch_size, 1), ddim_pre_time[i]).to(device=device) if i!=0 else None
+            pred_noise = model(xt, t)
+            pred_x0 = self._pred_x0_from_noise(xt, t, pred_noise)
+            if i!=0:
+                sigma_t = eta*torch.sqrt((1-self.cumprod_alpha_schedule[pre_t])/(1-self.cumprod_alpha_schedule[t]))*torch.sqrt(1-self.cumprod_alpha_schedule[t]/self.cumprod_alpha_schedule[pre_t])
+                sigma_t = sigma_t.reshape(-1, 1, 1, 1)
+                dir_point_xt = torch.sqrt(1-self.cumprod_alpha_schedule[pre_t].reshape(-1, 1, 1, 1)-sigma_t**2)*pred_noise
+                random_noise = torch.rand_like(sigma_t)
+                xt = torch.sqrt(self.cumprod_alpha_schedule[pre_t]).reshape(-1, 1, 1, 1)*pred_x0+dir_point_xt+sigma_t*random_noise
+            else:
+                xt = pred_x0
+        return xt
+
     def get_train_loss(self, model, x0, t):
         noise = torch.randn(x0.shape, device=x0.device)
         xt = self.q_xt_x0(x0, t, noise)
@@ -78,10 +100,11 @@ if __name__=="__main__":
     # t = torch.randint(1, 1000, (16, 1)).to(device=device)
     unet = UNetModel(num_head=1, channel_mul_layer=(1, 2, 2, 2), model_channels=32).to(device=device)
 
-    diffusion = GaussianDiffusion(device=device, time_steps=500)
+    diffusion = GaussianDiffusion(device=device, time_steps=1000)
     # loss = diffusion.get_train_loss(unet, x0, t)
     # print(loss)
-    img = diffusion.ddpm_fasion_sample(unet, 64, 3, 16)
+    #img = diffusion.ddpm_fasion_sample(unet, 64, 3, 16)
+    img = diffusion.ddim_fashion_sample(unet, 32, 3, 50)
     print(img.shape)
 
 
